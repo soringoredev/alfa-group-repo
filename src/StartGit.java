aa1652168b60f6957fa437b857f5ebc892971dd9
Sorin Gore <soringoredev@gmail.com>
2025-12-06 17:01:51 +0200

    Prepare clas for git workflow

---
+++ src/StartGit.java
@@ -0,0 +1,25 @@
+public class StartGit {
+    public static void main(String[] args) {
+
+        System.out.println("Sorin Gore");
+
+        //Igor
+
+
+        // Catalin
+
+
+        // Nadea
+
+
+        // Cristina
+
+
+        // Carolina
+
+
+        // Viorel
+
+
+    }
+}
